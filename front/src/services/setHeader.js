26d08a0a6c168e41deec8c4588ffbd94d5980d26
= <pierre.alexis.blond@gmail.com>
2019-08-19 12:17:49 +0200

    add SetHeader component and methods

--- front/src/services/setHeader.js
+++ front/src/services/setHeader.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export default async ({ name, title, subtitle }) => {
   const response = await axios
     .get(
-      `http://localhost:1337/graphql?query={
+      `https://fir-fiverr-a2e6b.appspot.com/graphql?query={
             setHeader(name: "${name}", title: "${title}", subtitle: "${subtitle}") {      
                 name
                 title
