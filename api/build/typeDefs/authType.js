80c740163f03421f70390527e163798eb137ac3c
= <pierre.alexis.blond@gmail.com>
2019-08-22 16:11:51 +0200

    remove build directory

--- api/build/typeDefs/authType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var _default = "\ntype User {\n  id: Int!\n  username: String!\n  email: String!\n}";
-exports["default"] = _default;
--- api/build/typeDefs/contactType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var _default = "\ntype Contact {\n  _id: String,\n  email: String,\n  name: String,\n  content: String,\n  at: String,\n  isRead: Boolean\n}";
-exports["default"] = _default;
