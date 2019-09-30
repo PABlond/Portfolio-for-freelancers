558eb95af130d4e950e0831467eb030d5934ff9d
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-09-30 14:56:16 +0200

    query data into page

--- front/gatsby-config.js
+++ front/gatsby-config.js
@@ -1,8 +1,12 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `PABlond portfolio`,
+    description: `PABlond portfolio | generated with CMS for freelancer by Pierre-Alexis Blond`,
+    author: `Pierre-Alexis Blond`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,19 +22,21 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-apollo",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        uri: `${process.env.API_URL}/graphql`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "PABlondAPI",
+        // This is field under which it's accessible
+        fieldName: "API",
+        // Url to query from
+        url: `${process.env.API_URL}/graphql`,
+      },
+    },
   ],
 }
