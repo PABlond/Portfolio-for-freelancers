5f905f87f13cd279fa255d8393e699539a88202a
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-03 16:55:43 +0200

    test with netlify /7

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react"
 import { Container, Form, Button } from "react-bootstrap"
-import NetlifyForm from "react-netlify-form"
 
 export default () => {
   const [form, setValues] = useState({
@@ -42,71 +41,35 @@ export default () => {
   return (
     <Container fluid>
       <h1>Want to hire me ?</h1>
-      <NetlifyForm
-        name="Form With Recaptcha"
-        recaptcha={{
-          sitekey: "6Le9tbsUAAAAAB0vewi1XAF9aTG1vd6QYmTO7OxP",
-          size: "normal",
-        }}
+      <form
+        name="contact"
+        method="post"
+        action="/success"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
       >
-        {({ loading, error, recaptchaError, success, recaptcha }) => (
-          <div>
-            {loading && <div>Loading...</div>}
-            {error && (
-              <div>Your information was not sent. Please try again later.</div>
-            )}
-            {recaptchaError && (
-              <div>
-                Recaptcha did not match. Please make sure the box is checked.
-              </div>
-            )}
-            {success && <div>Thank you for contacting us!</div>}
-            {!loading && !success && (
-              <>
-                <Form.Group>
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control
-                    type="email"
-                    name="email"
-                    placeholder="name@example.com"
-                    value={form.email}
-                    onChange={updateField}
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="name"
-                    placeholder="John Doe"
-                    value={form.name}
-                    onChange={updateField}
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Message</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    name="content"
-                    rows="3"
-                    value={form.content}
-                    onChange={updateField}
-                  />
-                </Form.Group>
-                {recaptcha}
-                <Button
-                  block
-                  type="submit"
-                  id="contact-button"
-                  variant="danger"
-                >
-                  SUBMIT
-                </Button>
-              </>
-            )}
-          </div>
-        )}
-      </NetlifyForm>
+        <input type="hidden" name="bot-field" />
+        <div className="field half first">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div className="field half">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email" />
+        </div>
+        <div className="field">
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" rows="6"></textarea>
+        </div>
+        <ul className="actions">
+          <li>
+            <input type="submit" value="Send Message" className="special" />
+          </li>
+          <li>
+            <input type="reset" value="Clear" />
+          </li>
+        </ul>
+      </form>
       {/* <Form
         data-netlify="true"
         onSubmit={onSubmit}
