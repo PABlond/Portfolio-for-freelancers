359cfa0883dce19a310b18d58f1d51aef8b4c79f
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-03 13:29:44 +0200

    test with netlify form /1

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { Container, Form, Button } from "react-bootstrap"
+import NetlifyForm from "react-netlify-form"
 
 export default () => {
   const [form, setValues] = useState({
@@ -26,47 +27,71 @@ export default () => {
   return (
     <Container fluid>
       <h1>Want to hire me ?</h1>
-      <Form onSubmit={onSubmit} className="offset-md-3 col-md-6">
-        <Form.Group>
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            placeholder="name@example.com"
-            value={form.email}
-            onChange={updateField}
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="name"
-            placeholder="John Doe"
-            value={form.name}
-            onChange={updateField}
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Message</Form.Label>
-          <Form.Control
-            as="textarea"
-            name="content"
-            rows="3"
-            value={form.content}
-            onChange={updateField}
-          />
-        </Form.Group>
-        <Button
-        block
-          type="submit"
-          id="contact-button"
-          variant="danger"
-        >
-          SUBMIT
-        </Button>
-
-      </Form>
+      <NetlifyForm
+        name="Form With Recaptcha"
+        recaptcha={{
+          sitekey: "6LfvtLsUAAAAALesUzQBYaDxx06yU8303Y2N-AUa",
+          size: "normal",
+        }}
+      >
+        {({ loading, error, recaptchaError, success, recaptcha }) => (
+          <div>
+            {loading && <div>Loading...</div>}
+            {error && (
+              <div>Your information was not sent. Please try again later.</div>
+            )}
+            {recaptchaError && (
+              <div>
+                Recaptcha did not match. Please make sure the box is checked.
+              </div>
+            )}
+            {success && <div>Thank you for contacting us!</div>}
+            {!loading && !success && (
+              <>
+                <Form.Group>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    placeholder="name@example.com"
+                    value={form.email}
+                    onChange={updateField}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="name"
+                    placeholder="John Doe"
+                    value={form.name}
+                    onChange={updateField}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Message</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    name="content"
+                    rows="3"
+                    value={form.content}
+                    onChange={updateField}
+                  />
+                </Form.Group>
+                {recaptcha}
+                <Button
+                  block
+                  type="submit"
+                  id="contact-button"
+                  variant="danger"
+                >
+                  SUBMIT
+                </Button>
+              </>
+            )}
+          </div>
+        )}
+      </NetlifyForm>
     </Container>
   )
 }
