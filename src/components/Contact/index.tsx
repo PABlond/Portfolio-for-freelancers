8cccad7bb495e952319fd443b464745873b64d8e
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-03 16:16:55 +0200

    test with netlify form /6

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { Container, Form, Button } from "react-bootstrap"
+import NetlifyForm from "react-netlify-form"
 
 export default () => {
   const [form, setValues] = useState({
@@ -41,21 +42,71 @@ export default () => {
   return (
     <Container fluid>
       <h1>Want to hire me ?</h1>
-      <form
-        name="JSX Form"
-        method="POST"
-        data-netlify="true"
-        data-netlify-recaptcha="true"
+      <NetlifyForm
+        name="Form With Recaptcha"
+        recaptcha={{
+          sitekey: "6Le9tbsUAAAAAB0vewi1XAF9aTG1vd6QYmTO7OxP",
+          size: "normal",
+        }}
       >
-        <input type="hidden" name="form-name" value="JSX Form" />
-        <label>Your Email:</label>
-        <input type="email" name="email" />
-        <br />
-        <label>Message:</label>
-        <textarea name="message" />
-        <br />
-        <button type="submit">Send</button>
-      </form>
+        {({ loading, error, recaptchaError, success, recaptcha }) => (
+          <div>
+            {loading && <div>Loading...</div>}
+            {error && (
+              <div>Your information was not sent. Please try again later.</div>
+            )}
+            {recaptchaError && (
+              <div>
+                Recaptcha did not match. Please make sure the box is checked.
+              </div>
+            )}
+            {success && <div>Thank you for contacting us!</div>}
+            {!loading && !success && (
+              <>
+                <Form.Group>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    placeholder="name@example.com"
+                    value={form.email}
+                    onChange={updateField}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="name"
+                    placeholder="John Doe"
+                    value={form.name}
+                    onChange={updateField}
+                  />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Message</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    name="content"
+                    rows="3"
+                    value={form.content}
+                    onChange={updateField}
+                  />
+                </Form.Group>
+                {recaptcha}
+                <Button
+                  block
+                  type="submit"
+                  id="contact-button"
+                  variant="danger"
+                >
+                  SUBMIT
+                </Button>
+              </>
+            )}
+          </div>
+        )}
+      </NetlifyForm>
       {/* <Form
         data-netlify="true"
         onSubmit={onSubmit}
