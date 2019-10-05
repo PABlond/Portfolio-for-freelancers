9ed4f9e60f08b14acaf1c921a540d4a7e9c1bab2
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-05 11:57:03 +0200

    add gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     "gatsby-plugin-typescript",
     `gatsby-plugin-sass`,
+    `gatsby-plugin-netlify`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
   ],
