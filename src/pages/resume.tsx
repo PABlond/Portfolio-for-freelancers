76cdbdb35afc9d347462e27e77082eaa0e360655
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-07 11:55:19 +0200

    create helpers for resume page

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -3,65 +3,21 @@ import { graphql } from "gatsby"
 import Resume from "./../components/Resume"
 import { PDFViewer } from "@react-pdf/renderer"
 import { connect } from "react-redux"
-import { IMdNode, IImageQuery } from "./../interfaces/query.interface"
+import formatResumePage from "./../helpers/resumeHelpers"
 import { IState } from "./../interfaces/state.interface"
 import { dispatchResume } from "./../config/constants"
-import Loading from './../components/Loading'
+import Loading from "./../components/Loading"
 
 const ResumePage = ({ data, dispatchFullResume }: any) => {
-  const [works, setWorks] = useState([])
   const [loading, setLoading] = useState<Boolean>(true)
 
-  const formatWorks = works =>
-    works.map(work => {
-      const title = work.html.split("<h4>")[1].split("</h4>")[0]
-      const content = work.html.split("<h6>")[1].split("</h6>")[0]
-      const technos = work.html.split("<p>")[2].split("</p>")[0]
-      const __html = work.html
-      const n = parseInt(work.frontmatter.title.split("-")[1])
-      return { title, content, technos, __html, n }
-    })
-
-    // const formatAbout = about => 
-
-    const formatHeader = str => {
-        const name = str.split('<h1>')[1].split('</h1>')[0]
-        const title = str.split('<h2>')[1].split('</h2>')[0]
-        return {name, title}
-    }
-
   useEffect(() => {
-    const works = formatWorks(
-      data.allMarkdownRemark.edges
-        .map((mod: IMdNode) => {
-          return mod.node.frontmatter.title.indexOf("work-") !== -1
-            ? mod.node
-            : null
-        })
-        .filter(Boolean)
-    )
-
-    const about = data.allMarkdownRemark.edges
-    .map((mod: IMdNode) =>
-      mod.node.frontmatter.title == "about" ? mod.node.html : null
-    )
-    .filter(Boolean)[0].split('</p>').map(str => {
-
-        return str.split('<p>')[1]
-    }).slice(1).filter(Boolean)
-
-    const header = formatHeader(data.allMarkdownRemark.edges
-        .map((mod: IMdNode) =>
-          mod.node.frontmatter.title == "header" ? mod.node.html : null
-        )
-        .filter(Boolean)[0])
-        
-    
-    dispatchFullResume({ header, about, works })
+    dispatchFullResume(formatResumePage(data.allMarkdownRemark.edges))
     setLoading(false)
   }, [])
+  
   return !loading ? (
-    <PDFViewer  style={{ height: "100vh", width: "100%" }}>
+    <PDFViewer style={{ height: "100vh", width: "100%" }}>
       <Resume />
     </PDFViewer>
   ) : (
