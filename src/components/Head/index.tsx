6c99b2eee082e89c0ea9e55ab635003b7c77f68e
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-07 11:23:04 +0200

    Implement Head component

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -1,27 +1,24 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 
-export default () => (
+interface IHeadProps {
+  siteUrl: string
+  defaultTitle: string
+  defaultDescription: string
+  themeColor: string
+  pageName?: string
+}
+
+export default ({
+  data: { siteUrl, defaultDescription, defaultTitle, themeColor, pageName },
+}: {
+  data: IHeadProps
+}) => (
   <Helmet>
     <meta charSet="utf-8" />
-    <title>Pierre-Alexis Blond - Portfolio</title>
-    <meta
-      name="description"
-      content="Full Stack developer and data lover available for any purpose. I am currently specialized in Javascript and Python. "
-    />
-    <meta
-      name="keywords"
-      content="developer, full stack, react, redux, nodejs, node"
-    />
-    <meta charSet="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <link
-      rel="shortcut icon"
-      href="/icons/icon-48x48.png?v=008654519ce705ac7bc44303a9014606"
-    />
+    <title>{pageName || defaultTitle}</title>
+    <link rel="canonical" href={siteUrl} />
+    <meta name="description" content={defaultDescription} />
+    <meta name="theme-color" content={themeColor || "#fff"} />
   </Helmet>
 )
