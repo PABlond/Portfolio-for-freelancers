ecf05df4593255016a97fcb401b1304c0c194278
PABlond <pierre-alexis.blond@live.fr>
2020-03-16 21:04:08 +0100

    update src/data/skills.ts

---
+++ src/data/skills.ts
@@ -0,0 +1,72 @@
+export default {
+    frontEnd: [
+        "HTML5",
+        "CSS",
+        "SCSS",
+        "React.js",
+        "React Native",
+        "Redux",
+        "Gatsby",
+        "Next.js",
+        "Bootstrap",
+        "Javascript",
+        "Typescript",
+        "ES6"
+    ],
+    backEnd: [
+        "Node.js",
+        "Express",
+        "Hapi.js",
+        "Python",
+        "Flask",
+        "Django",
+        "AWS",
+        "Lamda",
+        "AppSync",
+        "Firebase",
+    ],
+    db: [
+        "MongoDB",
+        "MySQL",
+        "PostgreSQL",
+        "SQLite"
+    ],
+    qA: [
+        "Jest",
+        "Mocha",
+        "Chai",
+        "Enzyme",
+        "Selenium",
+        "Pupeteer"
+    ],
+    devOps: [
+        "AWS",
+        "EC2",
+        "S3",
+        "Route53",
+        "Lambda",
+        "IAM",
+        "Docker"
+    ],
+    devWorkFlow: [
+        "Git",
+        "Npm",
+        "Yarn",
+        "Docker",
+        "Webpack",
+        "Jira",
+        "Scrum",
+        "Kanban",
+        "TDD",
+        "OOP",
+        "MVC"
+    ],
+    api: [
+        "GraphQL",
+        "REST",
+        "SOAP",
+        "OAuth",
+        "JWT",
+        "JSON"
+    ]
+}
