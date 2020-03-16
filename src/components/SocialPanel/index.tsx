9341c8c9bd770082ae53df0b9a02d7ec0843c43e
PABlond <pierre-alexis.blond@live.fr>
2020-03-16 21:04:08 +0100

    update src/components/SocialPanel/index.tsx

---
+++ src/components/SocialPanel/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Row } from "react-bootstrap"
+import { FaLinkedin, FaGithubSquare, FaTwitterSquare } from "react-icons/fa"
+
+export default () => {
+  return (
+    <Row>
+      <FaLinkedin />
+      <FaGithubSquare />
+      <FaTwitterSquare />
+    </Row>
+  )
+}
