89f5895ba00874ca05421a2a37240bc40cf41586
PABlond <pierre-alexis.blond@live.fr>
2020-03-16 21:04:08 +0100

    update src/data/certifications.ts

---
+++ src/data/certifications.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: "APIs and Microservices - Freecodecamp",
+    filename: "certificationsapi_cert",
+  },
+  {
+    name: "IBM Data Science Professional Certificate - IBM",
+    filename: "data_science_cert",
+  },
+  { name: "Data Visualization - Freecodecamp", filename: "Data_viz" },
+  { name: "Full Stack - Freecodecamp", filename: "full_stack_cert" },
+  { name: "Front End Libraries - Freecodecamp", filename: "front_end_cert" },
+  {
+    name: "Information Security and Quality Assurance - Freecodecamp",
+    filename: "info_sec_cert",
+  },
+  {
+    name: "Javascript Algorithms and Data Structures - Freecodecamp",
+    filename: "js_alg_cert",
+  },
+  { name: "Legacy Front End - Freecodecamp", filename: "Leg-Front_end" },
+  {
+    name: "Responsibe Web Design - Freecodecamp",
+    filename: "resp_webdesign_cert",
+  },
+]
