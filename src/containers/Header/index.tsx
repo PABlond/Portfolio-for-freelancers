fd8af49739d61c5c68853e1202f76362e72ef908
PABlond <pierre-alexis.blond@live.fr>
2020-03-16 21:04:08 +0100

    update src/containers/Header/index.tsx

---
+++ src/containers/Header/index.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { header as headerContent } from "../../data/content"
+import { StaticQuery, graphql } from "gatsby"
+import Img, { FluidObject } from "gatsby-image"
+
+export default () => {
+  const { name, role, description, resume } = headerContent
+  return (
+    <section id="header">
+      <div id="header-heading" className="animated fadeInDownBig">
+        <h1>{name}</h1>
+        <h2>{role}</h2>
+      </div>
+      <div id="header-img">
+        <StaticQuery
+          query={graphql`
+            query HeaderImg {
+              file(relativePath: { eq: "header.jpeg" }) {
+                id
+                childImageSharp {
+                  fluid(maxWidth: 700) {
+                    ...GatsbyImageSharpFluid_noBase64
+                  }
+                }
+              }
+            }
+          `}
+          render={(data: {
+            file: { childImageSharp: { fluid: FluidObject } }
+          }) => <Img {...data.file.childImageSharp} />}
+        />
+      </div>
+
+      <div className="row">
+        <div id="header-text" className="offset-md-2 col-md-8">
+          <p>{description}</p>
+          <a>{resume}</a>
+        </div>
+      </div>
+    </section>
+  )
+}
