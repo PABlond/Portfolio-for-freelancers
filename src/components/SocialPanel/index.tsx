57694bc950718fc4370a873c92b4b93f52482b21
PABlond <pierre-alexis.blond@live.fr>
2020-03-17 10:43:58 +0100

    update social panel component

--- src/components/SocialPanel/index.tsx
+++ src/components/SocialPanel/index.tsx
@@ -5,9 +5,31 @@ import { FaLinkedin, FaGithubSquare, FaTwitterSquare } from "react-icons/fa"
 export default () => {
   return (
     <Row>
-      <FaLinkedin />
-      <FaGithubSquare />
-      <FaTwitterSquare />
+      <a
+        href="https://www.linkedin.com/in/pierre-alexis-blond-00924b158/"
+        target="blank"
+        style={{ color: "#0077B5" }}
+      >
+        <FaLinkedin size={32} />
+      </a>
+      <a
+        href="https://github.com/PABlond"
+        target="blank"
+        style={{
+          color: "#211F1F",
+          marginLeft: "1rem",
+          marginRight: "1rem",
+        }}
+      >
+        <FaGithubSquare size={32} />
+      </a>
+      <a
+        href="https://twitter.com/_pablond"
+        target="blank"
+        style={{ color: "#55acee" }}
+      >
+        <FaTwitterSquare size={32} />
+      </a>
     </Row>
   )
 }
