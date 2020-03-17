30fbc16b76d1b7e18150a37b88e17c948938f0c3
PABlond <pierre-alexis.blond@live.fr>
2020-03-17 18:55:44 +0100

    create config file

---
+++ install/config.py
@@ -0,0 +1,9 @@
+class Variable:
+    config = 'install/config.xls'
+    content_template = 'install/content.template'
+    gatsby_config_template = 'install/gatsby-config.template'
+    gatsby_config = 'gatsby-config.js'
+    content = 'src/data/content.ts'
+
+    open_source_xls = 'install/open-source.xls'
+    experiences_xls = 'install/experiences.xls'
